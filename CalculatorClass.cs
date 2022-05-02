3acc8db711ff1b3e8faa455e20161be5addae9b7
NizarAhamed1998 <93996784+NizarAhamed1998@users.noreply.github.com>
2022-05-02 19:51:37 +0530

    Add files via upload

---
+++ CalculatorClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+   public class CalculatorClass
+    {
+        double _total;
+
+        public double Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+        double _value;
+
+
+        public double Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+
+        public CalculatorClass()
+        {
+
+        }
+        public void addition(CalculatorClass obj)
+        {
+            CalculatorClass val = obj;
+            _total += val.Value;
+        }
+        public void subtract(CalculatorClass obj)
+        {
+            CalculatorClass val = obj;
+            _total -= val.Value;
+        }
+        public void multiply(CalculatorClass obj)
+        {
+            CalculatorClass val = obj;
+            _total *= val.Value;
+           
+        }
+        public void divide(CalculatorClass obj)
+        {
+            CalculatorClass val = obj;
+            _total /= val.Value;
+        }
+        public override string ToString()
+        {
+            return string.Format("Total  {0}", _total);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClass calc = new CalculatorClass();
+            string method = "";
+            string format = "";
+            calc.Total = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            format += calc.Total;
+            do
+            {
+                Console.Write(format);
+                method = Console.ReadLine();
+                Console.Clear();
+                Console.Write(format += method); 
+                calc.Value = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+                Console.Write(format += calc.Value); 
+                
+                if (method=="+")
+                {
+                    calc.addition(calc);
+                }
+                else if (method=="-")
+                {
+                    calc.subtract(calc);
+                }
+                else if (method=="*")
+                {
+                    calc.multiply(calc);
+                }
+                else if (method=="/")
+                {
+                    calc.divide(calc);
+                }
+            } while (ConsoleKey.Enter==Console.ReadKey().Key);
+            Console.Clear();
+            Console.WriteLine(format);
+            Console.WriteLine(calc);
+        }
+    }
+}
